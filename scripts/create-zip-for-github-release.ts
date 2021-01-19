7a6a6ed0ab3d5de19699f744c94edf1bf7ef5f02
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-01-20 06:29:53 +0900

    chore: めたねのおくすり.exe は .zip に含めないようにする

--- scripts/create-zip-for-github-release.ts
+++ scripts/create-zip-for-github-release.ts
@@ -4,7 +4,7 @@ import archiver from "archiver";
 
 const archive = archiver("zip", { zlib: { level: 9 } });
 
-["metanen0x0", "めたねのおくすり", "繧√◆縺ｭ縺ｮ縺翫￥縺吶ｊ"].forEach((name) => {
+["metanen0x0", "繧√◆縺ｭ縺ｮ縺翫￥縺吶ｊ"].forEach((name) => {
   archive.append(
     fs.createReadStream(path.resolve(__dirname, "../dist/metanen0x0.exe")),
     { name: `${name}.exe` }
