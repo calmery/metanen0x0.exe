e13058a69ea0bf8eac907e38bc6e1c07e51f67da
calmery <12670155+calmery@users.noreply.github.com>
2021-01-19 18:28:11 +0900

    build(babel): @emotion/react を使えるようにする

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,10 +1,32 @@
+/// <reference types="@emotion/react/types/css-prop"/>
+
+import { css, Global } from "@emotion/react";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./renderer.scss";
+
+const container = css`
+  color: red;
+`;
 
 // Components
 
-const Index: React.FC = () => <div>Hello World</div>;
+const Index: React.FC = () => (
+  <>
+    <Global
+      styles={css`
+        body {
+          align-items: center;
+          display: flex;
+          height: 100vh;
+          justify-content: center;
+          overflow: hidden;
+          width: 100vw;
+        }
+      `}
+    />
+    <div css={container}>Hello World</div>
+  </>
+);
 
 // Main
 
